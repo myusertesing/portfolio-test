a46eb34f262d7ebf5425d9eb76f23bb3e63ce9a1
Soeurn Rothana <soeurn.rothana@sabay.com>
2024-03-05 01:43:05 +0700

    update script

--- build_docker.sh
+++ build_docker.sh
@@ -7,12 +7,12 @@ read name
 
 echo "\nbuild image for $name"
 docker build -t $name -f Dockerfile .
-echo "\ndone building image for $name..."
+echo "\ndone building image..."
 
 
-echo "\n\nstarting push docker image for $name"
+echo "\n\nstarting push docker image"
 echo "input tag: "
-
 read tag
+
 docker push $name:$tag
-echo "\ndone push docker image for $name..."
+echo "\ndone pushing docker image..."
