28b7def631b8a5b302fd5b9e789b6a8e6c975901
myusertesing <my.usertesing@gmail.com>
2024-06-06 09:55:35 +0700

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -5,14 +5,16 @@ echo "$0 script is running..."
 echo "input repository: "
 read name
 
+# sample name: ghcr.io/myusertesing/portfolio-test:1.0.0
+
 echo "\nbuild image for $name"
 docker build -t $name -f Dockerfile .
 echo "\ndone building image..."
 
 
-echo "\n\nstarting push docker image"
-echo "input tag: "
-read tag
+# echo "\n\nstarting push docker image"
+# echo "input tag: "
+# read tag
 
-docker push $name:$tag
+docker push $name
 echo "\ndone pushing docker image..."
